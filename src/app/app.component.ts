b784b02ca5092e336691ada0fa468d131482844d
Nicolas Bauer <nicolasbauer@Nicolass-MacBook-Air.local>
2019-10-21 15:46:39 -0700

    Makes app-component interesting
    So we can document stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
+/**
+ * Main description.
+ * @description The main component module
+ * @hidePrivateMembers false
+ */
+
+
 import { Component } from '@angular/core';
 
+
+/**
+ * @description My function
+ */
+export function helloWorld() {
+  return 'hello!';
+}
+
+/**
+ * Main Component Class
+ * @description The main component class
+ * @param my thing
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dgeni-app';
+  protected readonly title: string = 'dgeni-app';
+
+  constructor(private thing) {
+    console.log('hello!');
+  }
+
+  /**
+   * @description This is the getTitleFunction
+   * @param special thing
+   */
+  public getTitle(special: boolean) {
+    return this.title;
+  }
 }
