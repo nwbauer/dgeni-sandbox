8ea53e84cec6daeae8414c72ade7a31d06ad069c
Nicolas Bauer <nicolasbauer@Nicolass-MacBook-Air.local>
2019-10-21 15:46:04 -0700

    Adds dgeni templates and main script

---
+++ docs/dgeni.js
@@ -0,0 +1,74 @@
+// Canonical path provides a consistent path (i.e. always forward slashes) across different OSes
+var path = require('canonical-path');
+
+var Package = require('dgeni').Package;
+
+// Create and export a new Dgeni package called dgeni-example. This package depends upon
+// the jsdoc and nunjucks packages defined in the dgeni-packages npm module.
+module.exports = new Package('dgeni-example', [
+  require('dgeni-packages/typescript'),
+  require('dgeni-packages/nunjucks')
+])
+
+// Configure our dgeni-example package. We can ask the Dgeni dependency injector
+// to provide us with access to services and processors that we wish to configure
+.config(function(
+  log,
+  readFilesProcessor,
+  templateFinder,
+  writeFilesProcessor,
+  readTypeScriptModules,
+  computePathsProcessor,
+  EXPORT_DOC_TYPES
+) {
+
+  const MODULES_DOCS_PATH = 'partials/modules';
+
+  computePathsProcessor.pathTemplates.push({
+    docTypes: ['module'],
+    outputPathTemplate: MODULES_DOCS_PATH + '/${path}/index.html',
+    pathTemplate: '/${id}.file',
+  });
+
+  computePathsProcessor.pathTemplates.push({
+    docTypes: EXPORT_DOC_TYPES,
+    outputPathTemplate: MODULES_DOCS_PATH + '/${path}/index.html',
+    pathTemplate: '${moduleDoc.path}/${name}.${docType}',
+  });
+
+  // Set logging level
+  log.level = 'info';
+
+  // Specify the base path used when resolving relative paths to source and output files
+  readFilesProcessor.basePath = path.resolve(__dirname, '..');
+
+  // Specify collections of source files that should contain the documentation to extract
+  readFilesProcessor.sourceFiles = [
+    {
+      include: 'src/**/*.js',
+      basePath: 'src'
+    }
+  ];
+
+  // Specify the base path used when resolving relative paths to source and output files
+  readTypeScriptModules.basePath = path.resolve(__dirname, '..');
+
+  readTypeScriptModules.sourceFiles = [
+    {
+      include: 'src/**/*.ts',
+      exclude: 'src/**/*.spec.js',
+    }
+  ];
+
+  readTypeScriptModules.hidePrivateMembers = false;
+
+  // Add a folder to search for our own templates to use when rendering docs
+  templateFinder.templateFolders.unshift(path.resolve(__dirname, 'templates'));
+
+  // Specify how to match docs to templates.
+  // In this case we just use the same static template for all docs
+  templateFinder.templatePatterns.unshift('<%= doc.docType %>.template.html');
+
+  // Specify where the writeFilesProcessor will write our generated doc files
+  writeFilesProcessor.outputFolder  = 'build';
+});
