4e7e5b0c248020b9c0ed7df24b3141d64a342823
Nicolas Bauer <nicolasbauer@Nicolass-MacBook-Air.local>
2019-10-27 16:56:21 -0700

    Adds processor logic for Ng Component and Service

--- docs/src/processors/composeAngularDocs.ts
+++ docs/src/processors/composeAngularDocs.ts
@@ -1,19 +1,114 @@
 import { Processor, DocCollection, Document } from 'dgeni';
 import { ClassExportDoc } from 'dgeni-packages/typescript/api-doc-types/ClassExportDoc';
 import { AngularComponentDoc } from '../api-doc-types/angularComponentDoc';
+import { AngularServiceDoc } from '../api-doc-types/angularServiceDoc';
+import { MemberDoc } from 'dgeni-packages/typescript/api-doc-types/MemberDoc';
+import { MethodMemberDoc } from 'dgeni-packages/typescript/api-doc-types/MethodMemberDoc';
+import { ParsedDecorator } from 'dgeni-packages/typescript/services/TsParser/getDecorators';
+import { PropertyMemberDoc } from 'dgeni-packages/typescript/api-doc-types/PropertyMemberDoc';
 
 export function composeAngularDocs() {
   return new ComposeAngularDocs();
 }
 
+export type AngularDoc = AngularComponentDoc | AngularServiceDoc;
+
+export interface DocParseStrategy {
+  canParse: (doc: ClassExportDoc) => boolean;
+  parse: (doc: ClassExportDoc) => AngularDoc;
+}
+
+export function hasDecorator(doc: ClassExportDoc, name: string): boolean {
+  if (!doc.decorators) { return false; }
+  return doc.decorators.some((decorator: ParsedDecorator) => {
+    return decorator.name === name;
+  });
+}
+
 export class ComposeAngularDocs implements Processor {
-  $runAfter = ['mergeParameterInfo'];
+  $runAfter = ['adding-extra-docs'];
+  $runBefore = ['extra-docs-added'];
+
+  private parsers: DocParseStrategy[] = [
+    {
+      canParse: (doc: ClassExportDoc) => hasDecorator(doc, 'Component'),
+      parse: (doc: ClassExportDoc) => this.parseAngularComponent(doc)
+    },
+    {
+      canParse: (doc: ClassExportDoc) => hasDecorator(doc, 'Injectable'),
+      parse: (doc: ClassExportDoc) => this.parseAngularService(doc)
+    },
+    // {
+    //   canParse: (doc: ClassExportDoc) => hasDecorator(doc, 'NgModule'),
+    //   parser: (doc: ClassExportDoc) => this.parseAngularService(doc)
+    // }
+  ];
+
   $process(docs: DocCollection) {
     console.log('running ComposeAngularDocs');
-    docs.forEach((doc: Document) => {
+    return docs.map((doc: Document) => {
       if (doc instanceof ClassExportDoc) {
-        console.log('found class!');
+        const docStrategy = this.parsers.find(({canParse}) => canParse(doc));
+        return docStrategy ?  docStrategy.parse(doc) : doc;
       }
+      return doc;
     });
+  }
+
+  private parseAngularComponent(doc: Document): AngularComponentDoc {
+    const result = new AngularComponentDoc(
+      doc.host,
+      doc.moduleDoc,
+      doc.symbol,
+      doc.aliasSymbol
+    );
+
+    if (doc.members) {
+      doc.members.forEach((member: MemberDoc) => {
+
+        if (member.decorators) {
+          member.decorators.forEach((decorator: ParsedDecorator) => {
+            if (decorator.name === 'Input') {
+              result.inputs.push(member);
+            }
+            if (decorator.name === 'Output') {
+              result.outputs.push(member);
+            }
+          });
+        }
+
+      });
+    }
+
+    return result;
+  }
+
+
+  private parseAngularService(doc: Document): AngularServiceDoc {
+    const result = new AngularServiceDoc(
+      doc.host,
+      doc.moduleDoc,
+      doc.symbol,
+      doc.aliasSymbol
+    );
+
+    if (doc.members) {
+      doc.members.forEach((member: MemberDoc) => {
+
+        if (member.decorators) {
+          member.decorators.forEach((decorator: ParsedDecorator) => {
+            if (member instanceof MethodMemberDoc) {
+              result.methods.push(member);
+            }
+            if (member instanceof PropertyMemberDoc) {
+              result.properties.push(member);
+            }
+          });
+        }
+
+      });
+    }
+
+    return result;
   }
 }
